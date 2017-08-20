656f31bc86ff2394281bcb749e5ebec518a69fc4
medvedieva.antonina <medvedieva.antonina@gmail.com>
2017-08-20 17:08:13 +0300

    Merged implementing new logic master

---
+++ ls P2.js
@@ -0,0 +1,29 @@
+[33mcommit 4650751f4b501fd16dde018c0101316d2febe560[m[33m ([m[1;36mHEAD -> [m[1;32mmaster[m[33m, [m[1;31morigin/implement-new-logic[m[33m, [m[1;32mimplement-new-logic[m[33m)[m
+Author: medvedieva.antonina <medvedieva.antonina@gmail.com>
+Date:   Sun Aug 20 16:42:05 2017 +0300
+
+    Adde getPerson function
+
+[33mcommit 5eaa39b37e07c38d3c081c58e90de5b7b5e79269[m[33m ([m[1;31morigin/master[m[33m)[m
+Author: medvedieva.antonina <medvedieva.antonina@gmail.com>
+Date:   Sun Aug 20 15:25:44 2017 +0300
+
+    Added file P2.js
+
+[33mcommit 85407bc2755df2e9acaa25a3dbb433db5d7d697c[m
+Author: medvedieva.antonina <medvedieva.antonina@gmail.com>
+Date:   Sun Aug 20 14:46:03 2017 +0300
+
+    changed addNumber to deductNumber
+
+[33mcommit e3f04a3501a10d987c494885e84fcb6df2744bd0[m
+Author: medvedieva.antonina <medvedieva.antonina@gmail.com>
+Date:   Sun Aug 20 14:37:39 2017 +0300
+
+    Added addNumber functon
+
+[33mcommit 5eef71e980ff7d53992eb20d1a6d9e7cf4f2a8b2[m
+Author: medvedieva.antonina <medvedieva.antonina@gmail.com>
+Date:   Sun Aug 20 14:09:06 2017 +0300
+
+    First Commit
